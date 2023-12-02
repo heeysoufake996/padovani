18cbc39a80a71aa9266aed56627d2821a199ae0b
heeysoufake996 <139927767+heeysoufake996@users.noreply.github.com>
2023-12-02 12:39:53 -0300

    Delete minecraft directory

--- minecraft/Main Website/ler-mais.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('ler-mais').addEventListener('click', function() {
-    document.getElementById('texto-inicial').style.display = 'none';
-    document.getElementById('texto-restante').style.display = 'block';
-});
-
-document.getElementById('ler-menos').addEventListener('click', function() {
-    document.getElementById('texto-inicial').style.display = 'block';
-    document.getElementById('texto-restante').style.display = 'none';
-});
--- minecraft/README 2 - Template.md
+++
@@ -1,2 +0,0 @@
-# Template-Website-Undangan-Pernikahan
-Template Website Undangan Pernikahan By cripcips.net, free to download
